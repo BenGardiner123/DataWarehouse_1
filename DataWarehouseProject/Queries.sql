df4a75ff4268dc5d50272f2df20387578e7d8777
Ben Gardiner <bennygards@gmail.com>
2021-04-21 14:37:47 +1000

    query 8.1 complete - ref:ahlam for help with this

--- DataWarehouseProject/Queries.sql
+++ DataWarehouseProject/Queries.sql
@@ -1,1 +1,14 @@
-﻿
+﻿--PART 8 QUERIES
+--Write SQL queries based on your data warehouse to display the following information.
+--Note: The values used to create these examples will not match current values in the database tables. 
+
+-- LIST EACH WEEKDAY AND THE TOTAL SALES (QTY * SALEPRICE) IN DESC TOTAL SEQ
+
+
+SELECT DD.DayName AS [WEEKDAY], SUM((DS.QTY) * DS.SALEPRICE) AS [TOTAL SALES]
+FROM DWSALE DS
+LEFT JOIN DWDATE DD
+ON DS.SALEPRICE = DD.DATEKEY
+WHERE DD.DayName IS NOT NULL
+GROUP BY DD.DayName
+ORDER BY [TOTAL SALES]
